a8fb7b572955da55725a7da2c9c4aad455d002c6
teabuff@outlook.com <teabuff@outlook.com>
2025-05-03 16:30:37 -0700

    fix the nvim and zsh config

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -6,6 +6,19 @@ return {
       require "configs.conform"
     end,
   },
+  {
+     "nvchad/ui",
+      config = function()
+        require "nvchad"
+      end
+  },
+  {
+    "nvchad/base46",
+    lazy = true,
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
 
   -- These are some examples, uncomment them if you want to see them work!
   -- {
